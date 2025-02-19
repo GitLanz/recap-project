72ea6ed1f92347094f915209700366a1ddd950ad
Brigitte Lanz <brigitte.lanz.neuefische@gmail.com>
2025-02-19 12:08:59 +0100

    toggle bookmark button

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // phew… not a lot going on here. Please add some code!
+console.clear();
+
+const toggleBookmark = document.querySelector('[data-js="bookmark"]');
+toggleBookmark.addEventListener("click", () => {
+  //   event.preventDefault(); // only valid for a form to prevent instant submission of the browser
+  console.log("test");
+  console.log(toggleBookmark);
+  console.log(toggleBookmark.classList);
+  toggleBookmark.classList.toggle("bookmark--active");
+});
